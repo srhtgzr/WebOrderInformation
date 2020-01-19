982d3c8ca0a2119b39a9a44175d745f07a54a694
srhtgzr <serhatgezer89@gmail.com>
2020-01-19 16:06:25 -0600

    regression runner

---
+++ src/test/java/com/weborders/runners/RegressionRunner.java
@@ -0,0 +1,22 @@
+package com.weborders.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions (
+        features ={"src/test/resources/feature/login",
+                "src/test/resources/feature/view_all_orders"
+        } ,
+        glue = "src/test/step_definitions",
+        plugin = {"html:target/default-cucumber-reports",
+                "json:target/cucumber.json"
+        }
+)
+
+public class RegressionRunner {
+
+
+}
+
